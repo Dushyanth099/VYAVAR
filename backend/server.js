b6f1b837c89fd56879675183a2b1bf4749369bb5
dushyanth_099 <dushyanth0699@gmail.com>
2025-06-04 12:54:50 +0530

    frontend issue

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // Adjust for your frontend's URL
+    origin: ["https://tiscon.vercel.app"], // Adjust for your frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
